f9fa65e81f50a390033678bc17fc13476a88c433
T4rp <70074645+T4rp@users.noreply.github.com>
2025-07-28 10:37:25 -0400

    fix: make ts compile for windows somehow

--- build.rs
+++ build.rs
@@ -1,7 +1,10 @@
 use cmake::Config;
 
 fn main() {
-    let dst = cmake::build("./zlib");
+    let dst = Config::new("./zlib")
+        .define("BUILD_SHARED_LIBS", "FALSE")
+        .define("CMAKE_DEBUG_POSTFIX ", "")
+        .build();
 
     Config::new("./freetype")
         .define("DISABLE_FORCE_DEBUG_POSTFIX", "TRUE")
@@ -12,10 +15,25 @@ fn main() {
         .define("FT_DISABLE_BZIP2", "TRUE")
         .build();
 
+    let mut debug_suffix = "";
+
+    let target = std::env::var("TARGET").unwrap();
+    let libname = if target.contains("windows") && target.contains("msvc") {
+        if std::env::var("OPT_LEVEL").unwrap() == "0" {
+            debug_suffix = "d";
+        }
+        "zlibstatic"
+    } else {
+        "z"
+    };
+
+    println!("cargo:rustc-link-lib=static=z{}", debug_suffix);
+
     println!("cargo::rustc-link-search=native={}/lib", dst.display());
+    println!("cargo::rustc-link-search=native={}/bin", dst.display());
 
     println!("cargo::rustc-link-lib=static=freetype");
-    println!("cargo::rustc-link-lib=static=z");
 
-    println!("cargo::rerun-if-changed=build.rs");
+    println!("cargo::rerun-if-changed=zlib");
+    println!("cargo::rerun-if-changed=freetype");
 }

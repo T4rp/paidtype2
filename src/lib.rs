b611c4be2b5adcd922a63f85e31e8994bb21de1d
T4rp <why9ten@gmail.com>
2025-07-28 21:16:12 -0400

    fix: fix namespace conflict

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,10 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
-#[cfg_attr(target_os = "windows", path = "win_freetype.rs")]
+#[cfg(target_os = "windows")]
+#[path = "win_freetype.rs"]
 pub mod freetype;
 
 #[cfg(target_os = "linux")]
+#[path = "linux_freetype.rs"]
 pub mod freetype;
-

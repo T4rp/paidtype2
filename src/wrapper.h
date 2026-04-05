e9786a82fcea3da72247c8aa7a8a4b9b1d459c9f
T4rp <why9ten@gmail.com>
2026-04-05 16:57:48 -0400

    feat: add more freetype headers

--- src/wrapper.h
+++ src/wrapper.h
@@ -1,7 +1,7 @@
 #include <ft2build.h>
 #include FT_FREETYPE_H
-// #include <freetype2/freetype/freetype.h>
-// #include <freetype2/freetype/ftlcdfil.h>
-// #include <freetype2/freetype/tttables.h>
-// #include <freetype2/freetype/ftmodapi.h>
-// #include <freetype2/freetype/ftoutln.h>
+#include <freetype/fttypes.h>
+#include <freetype/fterrors.h>
+#include <freetype/ftglyph.h>
+#include <freetype/ftoutln.h>
+

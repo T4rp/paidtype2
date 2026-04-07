2b594570552493ba35331bfad2efe94f58586217
T4rp <why9ten@gmail.com>
2026-04-07 16:37:47 -0400

    feat: include ftbbox

--- src/wrapper.h
+++ src/wrapper.h
@@ -1,7 +1,7 @@
 #include <ft2build.h>
 #include FT_FREETYPE_H
-#include <freetype/fttypes.h>
+#include <freetype/ftbbox.h>
 #include <freetype/fterrors.h>
 #include <freetype/ftglyph.h>
 #include <freetype/ftoutln.h>
-
+#include <freetype/fttypes.h>
